5ada5d174da24fca8d2b9fcd794ad729f2fcac31
valentinarobledo <valenrobledo31@gmail.com>
2019-12-12 13:48:57 -0500

    Views students reports, workshop and information

---
+++ src/views/Asistencia.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="asistencia">
+		<div class="container">
+			
+			<div class="table">
+				<table>
+					<thead>
+						<tr>
+							<th></th>
+							<th>Estudiante</th>
+							<th>Código</th>
+							<th>Nota 1</th>
+							<th>Nota 2</th>
+							<th>Nota 3</th>
+							<th>Nota 4</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+	.asistencia{
+	background:#F0F0F0;
+	padding: 30px;
+	min-height: 100vh;
+}
+</style>
